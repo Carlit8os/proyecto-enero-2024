6b04eef9fd00582fca8af0832c9bac835e77889d
jeancarlos <jeanmartinez@gmail.com>
2024-01-27 23:05:07 -0500

    add: index

--- controller/veterinaria.js
+++ controller/veterinaria.js
@@ -15,11 +15,10 @@ class Veterinaria {
 
 const veterinaria1 = new Veterinaria(
   "Mr.Firulays", "Calle #53-24",3208897175);
-console.log(veterinaria1.printVeterinaria());
+
 
 function showVeterinaria() {
   let container = document.querySelector(".container-veterinaria");
-  console.log(container);
   container.innerHTML = `<div class="container"> ${veterinaria1.printVeterinaria()} </div>`;
 }
 showVeterinaria();
